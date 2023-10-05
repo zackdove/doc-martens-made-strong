357b8ffd287424bf81054b49019e9093f2c41988
Zack <zdove@live.co.uk>
2023-10-05 09:47:26 +0100

    Re-add styling

--- js/index.js
+++ js/index.js
@@ -85,17 +85,23 @@ function duplicateCards(count) {
   }
 }
 
-function setupCards() {
+const desktopCardMouseOverHandlers = [];
+const desktopCardMouseOutHandlers = [];
+const desktopCardClickHandlers = [];
+const desktopCardBackClickHandlers = [];
+
+function setupCardsDesktop() {
   const cards = document.getElementById("cards").children;
-  const offset = 64;
+  const offset = 56;
   const maxX = cards.length * offset;
   let hasFocus = false;
   for (let i = 0; i < cards.length; i++) {
+    console.log("redo");
     cards[i].style.transform = `translateX(${-(cards.length - i) * offset}px)`;
-    cards[i].addEventListener("mouseover", () => {
+    const mouseOverHandler = () => {
       // max is cards.length * offset;
       // loop through each, moving
-
+      console.log("mosueover");
       if (!hasFocus) {
         cards[i].classList.add("pointer");
         for (let j = 0; j < i + 1; j++) {
@@ -109,44 +115,212 @@ function setupCards() {
           }px)`;
         }
       }
-    });
-    cards[i].addEventListener("mouseout", () => {
+    };
+    desktopCardMouseOverHandlers.push(mouseOverHandler);
+    cards[i].addEventListener("mouseover", mouseOverHandler);
+
+    const mouseOutHandler = () => {
+      console.log("mouseout");
       if (!hasFocus) {
         cards[i].classList.remove("pointer");
         for (let i = 0; i < cards.length; i++) {
+          console.log("ee");
           cards[i].style.transform = `translateX(${
             -(cards.length - i) * offset
           }px)`;
         }
       }
-    });
-    cards[i].addEventListener("click", () => {
+    };
+    desktopCardMouseOutHandlers.push(mouseOutHandler);
+    cards[i].addEventListener("mouseout", mouseOutHandler);
+
+    const clickHandler = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log(hasFocus);
+      if (hasFocus) {
+        return;
+      }
+      console.log("click");
       hasFocus = true;
-      console.log("naughty");
+      const video = cards[i].querySelector("video");
+      console.log(video.dataset);
+      if (video.dataset.vidSrc) {
+        video.src = video.dataset.vidSrc;
+      }
+
       for (let j = 0; j < cards.length; j++) {
         cards[i].classList.remove("pointer");
         cards[j].style.transform = `translateX(${0}%)`;
       }
       cards[i].style.transform = `translateX(${-100}%)`;
-      // Load video here (or actually at start of this function)
-    });
-    console.log(cards[i]);
-    console.log(cards[i].querySelector(".cardBack"));
-    cards[i].querySelector(".cardBack").addEventListener("click", (e) => {
+      video.play();
+    };
+    desktopCardClickHandlers.push(clickHandler);
+    cards[i].addEventListener("mouseup", clickHandler);
+
+    const cardBackClickHandler = (e) => {
       e.preventDefault();
       e.stopPropagation();
-      console.log("hello");
+      console.log("back click");
+      const video = cards[i].querySelector("video");
+      video.currentTime = 0;
+      video.pause();
+      video.dataset.vidSrc = video.src;
+      video.src = "";
       hasFocus = false;
       for (let j = 0; j < cards.length; j++) {
-        console.log(j);
+        console.log("jejeje");
         cards[j].style.transform = `translateX(${
           -(cards.length - j) * offset
         }px)`;
       }
-    });
+    };
+    desktopCardBackClickHandlers.push(cardBackClickHandler);
+    cards[i]
+      .querySelector(".cardBack")
+      .addEventListener("mouseup", cardBackClickHandler);
   }
 }
-duplicateCards(12);
+
+const mobileCardClickHandlers = [];
+const mobileCardBackClickHandlers = [];
+
+function setupCardsMobile() {
+  const cards = document.getElementById("cards").children;
+  const offset = 56;
+  const maxY = cards.length * offset;
+
+  document.getElementById("mainContent").style.height = `${
+    maxY + document.getElementById("cards").offsetTop
+  }px`;
+
+  document.getElementById("main").style.overflowY = "scroll";
+  document.getElementById("mainContent").style.overflowY = "hidden";
+  let hasFocus = false;
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].style.transform = `translateY(${i * offset}px)`;
+
+    const clickHandler = (e) => {
+      console.log("mobile click");
+      if (hasFocus) {
+        cardBackClickHandler(e);
+
+        return;
+      }
+
+      const video = cards[i].querySelector("video");
+      console.log(video.dataset);
+      if (video.dataset.vidSrc) {
+        video.src = video.dataset.vidSrc;
+      }
+      video.play();
+      hasFocus = true;
+      // document.getElementById("main").scrollTo({ behavior: "smooth" });
+      const offsetY =
+        document.getElementById("main").scrollTop +
+        cards[0].getBoundingClientRect().top;
+      console.log(offsetY);
+      document.getElementById("main").scrollTo({ top: 0, behavior: "smooth" });
+
+      for (let j = 0; j < cards.length; j++) {
+        cards[j].classList.remove("pointer");
+        cards[j].style.transform = `translateY(${100}%)`;
+      }
+      cards[i].style.transform = `translateY(${-offsetY}px)`;
+      document.getElementById("mainContent").style.overflowY = "hidden";
+      document.getElementById(
+        "mainContent"
+      ).style.height = `${cards[i].offsetHeight}px`;
+      // Load video here (or actually at start of this function)
+    };
+    mobileCardClickHandlers.push(clickHandler);
+    cards[i].addEventListener("click", clickHandler);
+
+    const cardBackClickHandler = (e) => {
+      console.log("mobile back");
+      e.preventDefault();
+      e.stopPropagation();
+      const video = cards[i].querySelector("video");
+      video.currentTime = 0;
+      video.pause();
+      video.dataset.vidSrc = video.src;
+      video.src = "";
+      document.getElementById("main").scrollTo({ top: 0, behavior: "smooth" });
+      hasFocus = false;
+      for (let j = 0; j < cards.length; j++) {
+        cards[j].style.transform = `translateY(${j * offset}px)`;
+      }
+      document.getElementById("mainContent").style.height = `${
+        maxY + document.getElementById("cards").offsetTop
+      }px`;
+    };
+    mobileCardBackClickHandlers.push(cardBackClickHandler);
+    cards[i]
+      .querySelector(".cardBack")
+      .addEventListener("click", cardBackClickHandler);
+  }
+}
+
+function removeEventListeners() {
+  document.getElementById("mainContent").style.overflowY = "initial";
+  if (desktopCardMouseOverHandlers.length > 0) {
+    const cards = document.getElementById("cards").children;
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].removeEventListener(
+        "mouseover",
+        desktopCardMouseOverHandlers[i]
+      );
+      cards[i].removeEventListener("mouseout", desktopCardMouseOutHandlers[i]);
+      cards[i].removeEventListener("mouseup", desktopCardClickHandlers[i]);
+      cards[i]
+        .querySelector(".cardBack")
+        .removeEventListener("mouseup", desktopCardBackClickHandlers[i]);
+    }
+    desktopCardMouseOverHandlers.length = 0;
+    desktopCardMouseOutHandlers.length = 0;
+    desktopCardClickHandlers.length = 0;
+    desktopCardBackClickHandlers.length = 0;
+  }
+  if (mobileCardClickHandlers.length > 0) {
+    const cards = document.getElementById("cards").children;
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].removeEventListener("click", mobileCardClickHandlers[i]);
+      cards[i]
+        .querySelector(".cardBack")
+        .removeEventListener("click", mobileCardBackClickHandlers[i]);
+    }
+    mobileCardClickHandlers.length = 0;
+    mobileCardBackClickHandlers.length = 0;
+  }
+}
+
+function handleResize(e) {
+  console.log(e);
+  if (e.target.nodeName !== "VIDEO") {
+    removeEventListeners();
+    if (window.matchMedia("(max-width: 1200px").matches) {
+      setupCardsMobile();
+    } else {
+      setupCardsDesktop();
+    }
+  }
+}
+
+function setupCards() {
+  if (window.matchMedia("(max-width: 1200px").matches) {
+    window.onload = () => {
+      setupCardsMobile();
+    };
+  } else {
+    setupCardsDesktop();
+  }
+  window.addEventListener("resize", handleResize, true);
+}
+
+// remove this for prod
+// duplicateCards(13);
+
 setupCards();
 
 function showEventPopup() {
